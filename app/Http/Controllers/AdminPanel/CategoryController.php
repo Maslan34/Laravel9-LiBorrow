348fe46a1317f9524ab4ad8b350573592affe4e8
Maslan34 <muharrem5352@gmail.com>
2022-04-30 17:25:21 +0300

    SHOWING Data Category

---
+++ app/Http/Controllers/AdminPanel/CategoryController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\AdminPanel;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $data= Category::all();// getting data from data base
+        return view('admin.category.adminIndex',['data' => $data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new Category();
+        $data->title =$request->title;
+        $data->parent_id =0;
+        $data->keywords =$request->keywords;
+        $data->description =$request->description;
+        $data->status =$request->status;
+
+        $data->save();
+        return  redirect('/admin/category');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category,$id)
+    {
+        $data= Category::find($id);
+        return view('admin.category.show',['data' => $data]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category,$id)
+    {
+        $data= Category::find($id);// getting data from data base by using id
+        return view('admin.category.edit',['data' => $data]);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category,$id)
+    {
+        $data= Category::find($id);
+        $data->title =$request->title;
+        $data->keywords =$request->keywords;
+        $data->description =$request->description;
+        $data->status =$request->status;
+
+        $data->save();
+        return  redirect('/admin/category');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        echo "Category Destroy";
+    }
+}
